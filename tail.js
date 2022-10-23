51eda4268b136db06accc5a0f132b9a0541d3ab1
Dango3010 <thuyoanh199930@gmail.com>
2022-10-23 17:34:10 +0000

    code done

---
+++ tail.js
@@ -0,0 +1,23 @@
+let tail = (array) => {
+  let a = [];
+  for (let i = 1; i < array.length; i++) {
+    a.push(array[i]);
+  }
+  return a;
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`assertion passed`);
+  } else {
+    console.log(`🛑 ${actual} !== ${expected}`);
+  }
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+const empty = tail([]);
+
+assertEqual(result.length, 2);
+assertEqual(result[0], "Lighthouse");
+assertEqual(result[1], "Labs");
+assertEqual(empty.length, 2);

9c6da2350e6f92d983578b6636c5c8a3cda4ef07
Dango3010 <thuyoanh199930@gmail.com>
2022-11-10 18:27:44 +0000

    done

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,16 @@
 const assertArraysEqual = require('../assertArraysEqual'); //don't have to require eqArrays func as well.
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2]), []); // pass
-assertArraysEqual(middle([1, 2, 3, 8, 9]), [3]); // pass
-assertArraysEqual(middle([1, 2, 10, 4]), [2, 10]); // pass
+const assert = require('chai').assert;
 
+describe ('middle#', () => {
+  it('passes when returns empty array', () => {
+    assert.deepEqual(middle([1, 2]), [])
+  });
+  it('pass when returns 3', () => {
+    assert.deepEqual(middle([1, 2, 3, 8, 9]), [3]);
+  });
+  it('passes when returns 2,10', () => {
+    assert.deepEqual(middle([1, 2, 10, 4]), [2, 10]);
+  });
+});
